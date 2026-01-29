8a1e8ed998b0376c5f77580fba9fe9c1beb97e94
Jinjinov <dzindzinovic@yahoo.com>
2026-01-29 08:27:24 +0100

    fixed main

--- nas_sync_script_builder/__main__.py
+++ nas_sync_script_builder/__main__.py
@@ -1,10 +1,14 @@
 import sys
 
-if "--cli" in sys.argv:
-    # Remove the flag so argparse in cli.py works
-    sys.argv.remove("--cli")
-    from .cli import main as cli_main
-    cli_main()
-else:
-    from .gui import main as gui_main
-    gui_main()
+def main():
+    if "--cli" in sys.argv:
+        # Remove the flag so argparse in cli.py works
+        sys.argv.remove("--cli")
+        from .cli import main as cli_main
+        cli_main()
+    else:
+        from .gui import main as gui_main
+        gui_main()
+
+if __name__ == "__main__":
+    main()
